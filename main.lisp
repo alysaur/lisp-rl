c723f97f419dd6ab266caa8bff81fe3cb8cb632b
Alysaur <alysaur@users.noreply.github.com>
2016-07-04 23:05:59 -0700

    Change comments to doc strings where possible

--- main.lisp
+++ main.lisp
@@ -1,48 +1,49 @@
 ;;;; Alysaur trying to make a room.
 
-;;; Return random number between the specified low and high value.
 (defun random-from (low high)
+  "Return a random number between the specified low and high limits."
   (+ low (random (- high low -1))))
 
-;;; Make horizontal wall.
-;;;  e.g. "######"
 (defun make-wall (width)
+  "Make a horizontal wall of the specified width.
+    e.g. '######'"
   (make-list width :initial-element #\#))
 
-;;; Make room space.
-;;;  e.g. "...."
 (defun make-space-slice (width)
+  "Make a slice of empty space of the specified width.
+    e.g. '....'"
   (make-list width :initial-element #\.))
 
-;;; Make room space with walls.
-;;;  e.g. "#....#"
 (defun make-walled-slice (width)
+  "Make a slice of walled space of the specified width.
+    e.g. '#....#'"
   (nconc (cons #\# (make-space-slice (- width 2)))
 	  (list #\#)))
 
-;;; Make all room spaces with walls.
-;;;  e.g. "#....#"
-;;;       "#....#"
-;;;       "#....#"
 (defun make-walled-slices (width height)
+  "Make slices of walled space of the specified width and height.
+    e.g. '#....#'
+         '#....#'
+         '#....#'"
   (loop repeat height
      collect (make-walled-slice width)))
 
-;;; Make complete room.
-;;;  e.g. "######"
-;;;       "#....#"
-;;;       "#....#"
-;;;       "#....#"
-;;;       "######"
 (defun make-room (width height)
+  "Make a room of the specifed width and height.
+    e.g. '######'
+         '#....#'
+         '#....#'
+         '#....#'
+         '######'"
   (nconc (cons (make-wall width)
 		(make-walled-slices width (- height 2)))
 	  (list (make-wall width))))
 
 (defun make-random-room ()
-    (let ((width (random-from 3 50))
-	   (height (random-from 3 10)))
-      (make-room width height)))
+  "Make a room of random width and height."
+  (let ((width (random-from 3 50))
+	(height (random-from 3 10)))
+    (make-room width height)))
 
 ;;; Pretty print a room.
 (defun print-room (room)
